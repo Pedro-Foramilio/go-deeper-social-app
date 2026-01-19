46ef82c0ff19c9163c724c02da2fe8df77642619
Pedro-Foramilio <phcfor@gmail.com>
2026-01-19 20:54:05 -0300

    feat: add database configuration, migration scripts, and initial setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/Pedro-Foramilio/social/internal/db"
 	"github.com/Pedro-Foramilio/social/internal/env"
 	"github.com/Pedro-Foramilio/social/internal/store"
 	"github.com/joho/godotenv"
@@ -16,11 +17,32 @@ func main() {
 		log.Printf("Error loading envs: %v\n", err)
 	}
 
-	cfg := config{
-		addr: env.GetString("ADDR", ":8081"),
+	dbConfig := dbConfig{
+		addr:         env.GetString("DB_ADDR", "postgres://def:def@localhost/socialnetwork?sslmode=disable"),
+		maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+		maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+		maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 	}
 
-	store := store.NewStorage(nil)
+	cfg := config{
+		addr: env.GetString("ADDR", ":8081"),
+		db:   dbConfig,
+	}
+
+	db, err := db.New(
+		dbConfig.addr,
+		dbConfig.maxOpenConns,
+		dbConfig.maxIdleConns,
+		dbConfig.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panicf("Error connecting to the database: %v\n", err)
+	}
+	defer db.Close()
+	log.Println("Connected to the database successfully")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
